2e738083c9c54f711ce5b66d747cdd4a60c28de0
Sascha Wildgrube <sascha@wildgrube.com>
2020-10-21 18:33:28 +0200

    Added a chart with new daily infections / upgraded to WDK 0.0.0.75

--- _source/wdk/quality/tests/sourcefiles/test_deprecated.php
+++ _source/wdk/quality/tests/sourcefiles/test_deprecated.php
@@ -127,7 +127,9 @@
 						'/\-\>IncludeModule\((\"|\')security\/ssl(\"|\')\);/',
 						'/CCountdownElement/',
 						'/RenderCountdown/(/',
-						'/IsDevice/'
+						'/IsDevice/',
+						'/GetDatabaseConfigID/(/',
+						'/GetStage/(/'
 					);
 					
 				if ($strFileName != "wdk_url.inc")
